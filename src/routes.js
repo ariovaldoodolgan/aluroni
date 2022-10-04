29c0b66a24f06bb7c69e06787d352070672383dc
ariovaldo <ariovaldo@ituring.com.br>
2022-10-04 14:34:09 -0300

    criacao de uma pagina padrao, react-router-dom, Link

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
+import PaginaPadrao from 'components/paginaPadrao/PaginaPadrao';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Menu from 'components/menu';
 
 export default function AppRouter() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Inicio />} />
-				<Route path='/cardapio' element={<Cardapio />} />
-			</Routes>
-		</Router>
+		<main>
+			<Router>
+				<Menu />
+
+				<Routes>
+					<Route path='/' element={<PaginaPadrao />}>
+						<Route index element={<Inicio />} />
+						<Route path='cardapio' element={<Cardapio />} />
+					</Route>
+				</Routes>
+			</Router>
+		</main>
 	);
 }
